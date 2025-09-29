55ebf1e0b45dad054bf41bc19bd7966584d5342c
albserranog <105310020+serranxxx@users.noreply.github.com>
2025-09-28 21:41:21 -0600

    remove token on guests

--- router/guests.js
+++ router/guests.js
@@ -1,24 +1,24 @@
 const express = require('express');
 
-const { validarJWT } = require('../middlewares/validar-jwt');
+// const {} = require('../middlewares/validar-jwt');
 const { createGuest, getGuests, updateGuestInArray, deleteGuestInArray, getGuestByInvitationId, updateGuestByInvitationId, deleteGuestByInvitationId, getUpdatesByInvitationID, guestLogin, addShareItem, deleteShareItemById, shareLogin, confirmGuests } = require('../controllers/guests');
 
 const router = express.Router();
 
-router.post('/', validarJWT, createGuest)
-router.get('/', validarJWT, getGuests)
+router.post('/', createGuest)
+router.get('/', getGuests)
 router.get('/:id', getGuestByInvitationId)
 router.patch('/:id', updateGuestByInvitationId)
 router.patch('/confirm/:id', confirmGuests)
-router.delete('/:id', validarJWT, deleteGuestByInvitationId)
+router.delete('/:id', deleteGuestByInvitationId)
 
-router.patch('/:id/guests', validarJWT, updateGuestInArray)
-router.delete('/:id/guests', validarJWT, deleteGuestInArray)
-router.get('/:id/updates', validarJWT, getUpdatesByInvitationID)
+router.patch('/:id/guests', updateGuestInArray)
+router.delete('/:id/guests', deleteGuestInArray)
+router.get('/:id/updates', getUpdatesByInvitationID)
 
 router.post('/login', guestLogin)
-router.post('/shared/:id', validarJWT, addShareItem)
-router.delete('/shared/:id', validarJWT, deleteShareItemById)
+router.post('/shared/:id', addShareItem)
+router.delete('/shared/:id', deleteShareItemById)
 router.post('/shared/login/:id', shareLogin)
 
 module.exports = router;
